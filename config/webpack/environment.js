4549171e4462fa7f73e6db9d196f59650c63a955
junsikahn <junsikahn@vaskit.kr>
2018-02-22 04:45:51 +0900

    AJS webpacker 추가 (admin 에셋 오류 수정)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,17 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
 
+const config = require('@rails/webpacker/package/config')
+const ManifestPlugin = require('webpack-manifest-plugin')
+environment.plugins.append('Manifest', new ManifestPlugin({
+  publicPath: config.publicPath,
+  writeToFileEmit: true,
+  filter: f => {
+    f.name = f.name.replace(/\\/g, '/')
+    f.path = f.path.replace(/\\/g, '/')
+    return f
+  }
+}))
+
 environment.loaders.append('vue', vue)
 module.exports = environment
